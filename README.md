cb79da68e77e25843c685ca3ee88e63547a570c4
Juergen Pabel <juergen@pabel.net>
2021-06-11 21:03:28 +0200

    git reset'ed the repository to get rid of the 50MB french model and added current state of implementation as initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # kalliope-vosk
-implementation de VOSK pour Kalliope 
+Kalliope module for STT with VOSK (https://github.com/alphacep/vosk-api/)
+
+## Synopsis
+Speech-to-text in kalliope with VOSK (based on kaldi)
+
+## Preparation
+Before any module installation you have to check that you have defined the installation path for (at least SST) modules.
+To do this, you must modify the settings.yml file and activate the "resource_directory" by adapting the path (for at least sst).
+```bash
+resource_directory:
+  stt: "resources/stt"
+```
+
+## Install
+```bash
+kalliope install --git-url https://github.com/juergenpabel/kalliope-vosk.git
+```
+
+Change default STT to VOSK in settings.yml
+```yml
+default_speech_to_text: "vosk"
+```
+
+
+## Language model
+This repository is a fork of https://github.com/veka-server/kalliope-vosk; in that repository a basic french language model is included - this fork does not contain any language models at all.
+
+Download and install a language model you like (https://alphacephei.com/vosk/models). Unzip it and remember its path because you will have to add it to the settings.yml file afterwards. It is recommended to place the language model also in the corresponding resource directory for STT (see above).
+
+
+Change default STT to VOSK in settings.yml
+```yml
+  - vosk:
+      language: "<relative or absolute directory path to your language model>"
+```
+
+
+## Uninstall
+```bash
+kalliope uninstall --stt-name vosk
+pip3 uninstall vosk
+```
---
+++ vosk.py
@@ -0,0 +1,130 @@
+import speech_recognition as sr
+
+import sys
+import json
+import os
+import logging
+import re
+
+from kalliope.core import Utils
+from kalliope.core.ConfigurationManager import BrainLoader
+
+import importlib
+importlib.import_module('vosk')
+from vosk import Model, KaldiRecognizer, SetLogLevel
+
+from kalliope.stt.Utils import SpeechRecognition
+from speech_recognition import Microphone
+
+logging.basicConfig()
+logger = logging.getLogger("kalliope")
+
+
+class Vosk(SpeechRecognition):
+
+    def __init__(self, callback=None, **kwargs):
+        """
+        Start recording the microphone and analyse audio with Vosk
+        :param callback: The callback function to call to send the text
+        :param kwargs:
+        """
+
+        SpeechRecognition.__init__(self, kwargs.get('audio_file_path', None))
+        SetLogLevel(kwargs.get('log_level', -1))
+
+        self.main_controller_callback = callback
+        self.model_path = kwargs.get('model_path', None)
+        self.grammar_calculate = kwargs.get('grammar_calculate', True)
+
+        if self.grammar_calculate:
+            logger.debug("[vosk] generating kaldi grammar from all synapse orders and variables:")
+            self.grammar_calculated = []
+            brainloader = BrainLoader()
+            brain = brainloader.brain
+            for synapse in brain.synapses:
+                for signal in synapse.signals:
+                    if signal.name == "order":
+                        self.grammar_calculated.append(re.sub('{{.+}}', '', signal.parameters).lower())
+                        logger.debug("[vosk] - adding order '%s' to grammar" % signal.parameters.lower())
+            self._recursively_add_variable_leafs(brainloader.settings.variables)
+
+        klass = self.__class__
+        if not hasattr(klass, 'model'):
+            klass.model = {}
+        if self.model_path not in klass.model:
+            logger.debug("[vosk] model '%s' is not found in the class cache" % self.model_path)
+            if self.model_path is None or os.path.exists(self.model_path) == False:
+                Utils.print_danger("Please configure parameter 'model_path' to a valid language model")
+                exit (1)
+            klass.model[self.model_path] = Model(self.model_path)
+            logger.debug("[vosk] model '%s' is added to the class cache" % self.model_path)
+        if self.grammar_calculate:
+            for i, item in enumerate(self.grammar_calculated):
+                item = re.sub(r'[0-9!-/:-@[-`{-~]', ' ', item)
+                self.grammar_calculated[i] = item
+            self.grammar_calculated = list(set(" ".join(set(self.grammar_calculated)).split()))
+            self.grammar_calculated.sort()
+            logger.debug("[vosk] applying the calculated grammar for KaldiRecognizer: %s" % (self.grammar_calculated))
+            self.kaldirecognizer = KaldiRecognizer(klass.model[self.model_path], 16000, json.dumps(self.grammar_calculated, ensure_ascii=False))
+        else:
+            logger.debug("[vosk] using the grammar dictionary of the model for KaldiRecognizer")
+            self.kaldirecognizer = KaldiRecognizer(klass.model[self.model_path], 16000)
+        self.set_callback(self.vosk_callback)
+        self.start_processing()
+
+
+    def vosk_callback(self, recognizer, audio_data):
+        upm = sr.Microphone()
+        kl = sr.Recognizer()
+        sl_data = audio_data.get_raw_data(convert_rate=16000, convert_width=2)
+
+        try:
+            result = None
+            if not self.kaldirecognizer.AcceptWaveform(sl_data):
+                logger.debug("[vosk] KaldiRecognizer.AcceptWaveform() returned False, raising UnknownValueError")
+                raise sr.UnknownValueError
+            result = json.loads(self.kaldirecognizer.FinalResult())
+            logger.debug("[vosk] KaldiRecognizer.FinalResult() returned '%s'" % result['text'])
+            captured_audio = result['text']
+            Utils.print_success("Vosk thinks you said '%s'" % captured_audio)
+            self._analyse_audio(captured_audio)
+
+        except sr.UnknownValueError:
+            Utils.print_warning("Vosk Speech Recognition could not understand audio")
+            self._analyse_audio(audio_to_text=None)
+
+        except sr.RequestError as e:
+            Utils.print_danger("Could not request results from Vosk Speech Recognition service; %s" % (format(e)))
+            self._analyse_audio(audio_to_text=None)
+
+        except AssertionError:
+            Utils.print_warning("No audio caught from microphone")
+            self._analyse_audio(audio_to_text=None)
+
+
+    def _analyse_audio(self, audio_to_text):
+        """
+        Confirm the audio exists and run it in a Callback
+        :param audio_to_text: the captured audio
+        """
+        if self.main_controller_callback is not None:
+            self.main_controller_callback(audio_to_text)
+
+
+    def _recursively_add_variable_leafs(self, variables):
+        for variable in variables:
+            value = None
+            if isinstance(variables, dict):
+                value = variables[variable]
+                if isinstance(value, str):
+                    value = value.lower()
+                    logger.debug("[vosk] - adding '%s' from variables to grammar" % (value))
+                    self.grammar_calculated.append(value)
+                if isinstance(value, list) or isinstance(value, dict):
+                    self._recursively_add_variable_leafs(value)
+            if isinstance(variable, str):
+                value = variable.lower()
+                logger.debug("[vosk] - adding '%s' from variables to grammar" % (value))
+                self.grammar_calculated.append(value)
+            if isinstance(variable, list) or isinstance(variable, dict):
+                self._recursively_add_variable_leafs(variable)
